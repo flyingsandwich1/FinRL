e78c0bd85540216e17408e8ec6846246fe49557b
flyingsandwich1 <103616501+flyingsandwich1@users.noreply.github.com>
2022-05-16 00:55:11 +1000

    Update config.py

--- finrl/config.py
+++ finrl/config.py
@@ -34,15 +34,15 @@ INDICATORS = [
 
 
 # Model Parameters ARE ADDED TO MAIN TRADING FILE
-#A2C_PARAMS = {"n_steps": 5, "ent_coef": 0.01, "learning_rate": 0.0007}
-#PPO_PARAMS = {
- #   "n_steps": 2048,
-  #  "ent_coef": 0.01,
-   # "learning_rate": 0.00025,
-    #"batch_size": 64,
-#}
-#DDPG_PARAMS = {"batch_size": 128, "buffer_size": 50000, "learning_rate": 0.001}
-#TD3_PARAMS = {"batch_size": 100, "buffer_size": 1000000, "learning_rate": 0.001}
+A2C_PARAMS = {"n_steps": 5, "ent_coef": 0.01, "learning_rate": 0.0007}
+PPO_PARAMS = {
+    "n_steps": 2048,
+    "ent_coef": 0.01,
+    "learning_rate": 0.00025,
+    "batch_size": 64,
+}
+DDPG_PARAMS = {"batch_size": 128, "buffer_size": 50000, "learning_rate": 0.001}
+TD3_PARAMS = {"batch_size": 100, "buffer_size": 1000000, "learning_rate": 0.001}
 SAC_PARAMS = {
     "batch_size": 64,
     "buffer_size": 100000,
